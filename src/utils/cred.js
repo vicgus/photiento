39257868c5dff76526be0bcf6ce0fbe8f701572a
vicgus <victor.gsson@gmail.com>
2018-09-19 12:56:26 +0200

    Minor fix

--- src/utils/cred.js
+++ src/utils/cred.js
@@ -1,16 +1,3 @@
-// import Unsplash from 'unsplash-js';
-
-// const unsplash = new Unsplash({
-//   applicationId: 'a676542151d1ab845b6ad16706f62c7e51d86ea6ee532e45aa503be322957c23',
-//   secret: 'd72f14e73244007d66d80c2c54d41d7a7d901dae75448252fb629c202ea67972',
-// //   callbackUrl: env.CALLBACK_URL,
-// //   bearerToken: env.BEARER_TOKEN
-// });
-
-
-// window.unsplash=unsplash;
-// export default unsplash;
-
 const cred = {
     appId: 'a676542151d1ab845b6ad16706f62c7e51d86ea6ee532e45aa503be322957c23',
     secretKey: 'd72f14e73244007d66d80c2c54d41d7a7d901dae75448252fb629c202ea67972'
